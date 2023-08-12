9c448066bf8614f4f7752051d9160094047bb733
Janardhan Biharilal Yadav <63313078+janardhanyadav@users.noreply.github.com>
2023-08-12 21:23:27 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,8 +1,12 @@
 package com.driver;
 
 public class Main {
-  RWOnly obj = new RWOnly();
+  public static void main(String[] args){
+    RWOnly obj = new RWOnly();
 
   obj.setName("Janni");
 obj.getName();
+  }
+  
+  
 }
